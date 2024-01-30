a0addfbe7ed8d47da22c8efa2e8f1dcd727df74c
Aniket Kumar <125212655+ErAniketKumar@users.noreply.github.com>
2024-01-30 12:42:56 +0000

    dataType

---
+++ day02_basic/dataTypeSummary.js
@@ -0,0 +1,8 @@
+"use strict"
+//datatype summary
+
+
+//usally 7 type :   String,Number, Boolean, null,undefined,Object, Symbol, bigint
+//two type 
+//1 premitive:  String,Number, Boolean, null,undefined, Symbol, bigint;
+//2 nonPremitive  Array, Onject, function
