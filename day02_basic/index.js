c066b600a5cb454ac55fb6a83c80024938d4e2f7
Aniket Kumar <125212655+ErAniketKumar@users.noreply.github.com>
2024-01-30 12:37:48 +0000

    comparisionOperator

---
+++ day02_basic/index.js
@@ -0,0 +1,40 @@
+"use strict"
+//operation
+
+let value=3;
+let negvalue=-value;
+console.log(negvalue);
+console.log(2+value);
+console.log(value**value); //power value to the power value
+console.log(5**2); // square
+
+console.log(2+2+"2"); //2+2=4+"2"==42
+console.log(true);
+console.log(+true); //1 
+
+console.log(3**(true+true+true)); //3 ot the power (true->=1 so 1+1+1==3)-->> 3 to the power 3 -->27 ans
+
+let gameCounter=true;
+gameCounter++;
+console.log(gameCounter); //2; because true treated as 1 
+
+// comparision operator
+
+const x=1;
+console.log(2<=x)
+console.log(null==0);
+console.log(null<=0); //true;
+
+console.log(undefined==0) 
+console.log(undefined===null);
+console.log(null==false);
+console.log(2=="2");
+console.log(2==="2");
+
+
+
+
+
+
+
+
