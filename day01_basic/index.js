eefcd54b27a2c0341fa3cac7837fbad7fa4fcf0f
Aniket Kumar <125212655+ErAniketKumar@users.noreply.github.com>
2024-01-30 12:19:13 +0000

    basic data type

--- day01_basic/index.js
+++ day01_basic/index.js
@@ -1,1 +1,28 @@
-console.log("jai mata di");
+"use strict";
+const accountId=144553
+let accountEmail="aniket@gmail.com"
+var pass="12344";
+console.log(accountId);
+console.log(accountEmail);
+
+// alrt("hello"); this is use in broswer
+
+//data type
+/*
+String.
+Number.
+Bigint.
+Boolean.
+Undefined.
+Null.
+Symbol.
+Object.
+ */
+let x="hello";
+console.log(typeof x);
+
+/* typeof undefined-> undefined
+typeof null->object
+typeof array -> object */
+
+
