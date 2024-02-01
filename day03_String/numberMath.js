aa972fe1501f340b709ec62e076074d0816fdca6
Aniket Kumar <125212655+ErAniketKumar@users.noreply.github.com>
2024-02-01 15:04:41 +0000

    update Number and string in js

--- day03_String/numberMath.js
+++ day03_String/numberMath.js
@@ -1,7 +1,7 @@
 "use strict";
 
 //numbers and math;;
-// NUMBER
+//NUMBER
 
 const score=400;
 console.log(score);
@@ -24,5 +24,28 @@ console.log(mixval.toPrecision(2));
 //toLocalString
 
 console.log(mixval.toLocaleString()); // followed by the us standered i.e sperate by , like thousand lakhs and so more
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+//+++++++++++Maths +++++++++++++++//
+
+console.log(Math);
+console.log(Math.abs(-1)) // absolute value i.e negative also treat as posotive
+
+console.log(Math.round(4.6)) // roundoff
+console.log(Math.ceil(4.6)); // next to decimal value i.e 4.6--> 5;
+
+console.log(Math.floor(6.9)); // remove decimal i.e 6.9--> 6;
+
+// Math.min();
+// Math.max();
+// Math.pow();
+// Math.random(); lies between 0 to 1 i.e in decimal form
+
+
+const minval=10;
+const maxval=20;
+console.log(Math.floor(Math.random()*(maxval-minval +1) )+minval)
 
 
